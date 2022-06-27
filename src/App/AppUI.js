72f67ad2e1332c95a55c8bde6b618324f6846637
cetherdev <robert.hct@gmail.com>
2022-06-27 11:47:37 -0500

    feat: add useContext to manage the state around the application

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,25 +1,19 @@
 import React from 'react';
+import { TodoContext } from '../TodoContext';
 import { TodoCounter } from '../TodoCounter';
 import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 
-function AppUI({
-	loading,
-	error,
-	totalTodos,
-	completedTodos,
-	searchValue,
-	setSearchValue,
-	searchedTodos,
-	completeTodo,
-	deleteTodo,
-}) {
+function AppUI() {
+	const { error, loading, searchedTodos, completeTodo, deleteTodo } =
+		React.useContext(TodoContext);
 	return (
 		<React.Fragment>
-			<TodoCounter total={totalTodos} completed={completedTodos} />
-			<TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+			<TodoCounter />
+			<TodoSearch />
+
 			<TodoList>
 				{error && <p>Error al cargar</p>}
 				{loading && <p>Estamos cargando</p>}
@@ -34,6 +28,7 @@ function AppUI({
 					/>
 				))}
 			</TodoList>
+
 			<CreateTodoButton />
 		</React.Fragment>
 	);
